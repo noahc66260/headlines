37a291932f5aba31fec7b313ac5efaf00819f6af
Noah Ruderman <ruderman.noah@gmail.com>
2020-01-02 01:19:09 -0800

    Caching and CSS

--- headlines.py
+++ headlines.py
@@ -1,3 +1,5 @@
+import datetime
+from flask import make_response
 import json
 import urllib.request
 import urllib.parse
@@ -23,6 +25,14 @@ WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&
 
 CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=24e009325cd44d6584c5f58451515370"
 
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
+
+
 def get_rate(frm, to):
     all_currency = urllib.request.urlopen(CURRENCY_URL).read()
     parsed = json.loads(all_currency).get('rates')
@@ -33,30 +43,29 @@ def get_rate(frm, to):
 @app.route('/')
 def home():
     # get customized headlines, based on user input or default
-    publication = request.args.get('publication')
-    if not publication:
-        publication = DEFAULTS['publication']
+    publication = get_value_with_fallback('publication')
     articles = get_news(publication)
     # get customized weather based on user input or default
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_with_fallback('city')
     weather = get_weather(city)
     # get customized currency bassed on user input or default
-    currency_from = request.args.get("currency_from")
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get("currency_to")
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_with_fallback("currency_from")
+    currency_to = get_value_with_fallback("currency_to")
     rate, currencies = get_rate(currency_from, currency_to)
-    return render_template("home.html",
-                           articles=articles,
-                           weather=weather,
-                           currency_from=currency_from,
-                           currency_to=currency_to,
-                           rate=rate,
-                           currencies=sorted(currencies))
+    # save cookies and return template
+    response = make_response(render_template("home.html",
+                               articles=articles,
+                               weather=weather,
+                               currency_from=currency_from,
+                               currency_to=currency_to,
+                               rate=rate,
+                               currencies=sorted(currencies)))
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    response.set_cookie("currency_from", currency_from, expires=expires)
+    response.set_cookie("currency_to", currency_to, expires=expires)
+    return response
 
 def get_weather(query):
     query = urllib.parse.quote(query)
